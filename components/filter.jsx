38bd6a200dd7417a78b7a53660598741d67625f7
Anjy Gupta <anjyofficial@gmail.com>
2023-04-25 20:08:52 +0530

    added search bar and filter functionality

--- components/filter.jsx
+++ components/filter.jsx
@@ -1,15 +1,30 @@
+"use client"
+import { Cagliostro } from "next/font/google";
 import { useEffect } from "react";
 import React from "react";
 
-function Filter({ resources, setActiveOption, setFiltered, activeOption }) {
-  //   useEffect(() => {
-  //     if (activeOption === "all") {
-  //       setFiltered(resources);
-  //       return;
-  //     }
-  //     const filtered = resources.filter((card) => card.genre === activeOption);
-  //     setFiltered(filtered);
-  //   }, [activeOption, resources]);
+function Filter({ filtered , setFiltered, setActiveOption, activeOption, activeType, setActiveType, activeEcosystem, setActiveEcosystem }) {
+    // useEffect(() => {
+    //   if (activeOption === "all") {
+    //     setFiltered(resources);
+    //     return;
+    //   }
+    //   const filtered = resources.filter((card) => card.genre === activeOption);
+    //   setFiltered(filtered);
+    // }, [activeOption, resources]);
+  console.log("category",activeOption);
+  console.log("type",activeType);
+  console.log("ecosystem",activeEcosystem);
+  // console.log("hey",activeOption.length);
+  console.log("yoyo")
+
+  let category = ["AI", "Analytics", "Bridges/Interoperability", "CEX", "Communities", "Content", "DeFi", "Derivatives", "DEX", "Foundation", "GameFi", "Grants", "Index", "Infrastructure",
+    "Insurance", "IOT", "Layer 1", "Layer 2", "Lend/Borrow", "Metagovernance", "Music", "NFT", "Oracles", "Privacy", "Protocal DAO", "Quadratic Funding", "Research", "Social", "Social Causes",
+    "Stablecoin", "Staking", "Yield Farming"];
+
+  let type = ["Analytics", "Community", "Development", "Other"];
+
+  let ecosystem = ["Aurora", "Avalanche", "Bitcoin", "BNB", "Celo", "Cosmo Hub", "Ethereum", "Evmos", "Fantom", "Gnosis", "Harmony", "Internet Computer", "Katlyn", "NEAR Protocol", "Optimism", "Polygon", "Solana", "Stacks"];
 
   return (
     <div className="w-2/3 m-auto">
@@ -17,126 +32,24 @@ function Filter({ resources, setActiveOption, setFiltered, activeOption }) {
         <h1>Project Category</h1>
         <div className="flex flex-wrap flex-col sm:flex-row	">
 
-          <button
-            onClick={() => setActiveOption("Content")}
-            className={`${activeOption === "Content" ? "bg-slate-300 text-gray-600" : ""} bg-indigo-800  text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Content
-          </button>
-          <button
-            onClick={() => setActiveOption("DeFi")}
-            className={`${activeOption === "DeFi" ? "bg-slate-300 text-gray-600" : ""} bg-indigo-800  text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            DeFi
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-indigo-800  text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Derivatives
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-indigo-800  text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            DEX
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-indigo-800  text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Grants
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-indigo-800  text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            IOT
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-indigo-800  text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Layer 1
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-indigo-800  text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Layer 2
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-indigo-800  text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            NFT
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-indigo-800  text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Research
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-indigo-800  text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Lend/Borrow
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-indigo-800  text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Music
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-indigo-800  text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Staking
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-indigo-800  text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Stablecoin
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-indigo-800  text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Communities
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-indigo-800  text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Communities
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-indigo-800  text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Communities
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-indigo-800  text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Communities
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-indigo-800  text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Communities
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-indigo-800  text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Communities
-          </button>
+          {
+            category?.map((x) => {
+              return (<button
+                onClick={() => {
+                  if (activeOption?.includes(`${x}`)) {
+                    activeOption.splice(activeOption.indexOf(`${x}`), 1);
+                  }
+                  else {
+                    setActiveOption(prev => [...prev, `${x}`])
+                  }
+                }
+                }
+                className={`${activeOption?.includes(`${x}`) ? "bg-black text-gray-600" : "bg-indigo-800 "}  text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
+              >
+                {x}
+              </button>)
+            })
+          }
 
         </div>
       </div>
@@ -145,36 +58,26 @@ function Filter({ resources, setActiveOption, setFiltered, activeOption }) {
         <h1>Grants Type</h1>
         <div className="flex flex-wrap flex-col sm:flex-row	">
 
-          <button
-            onClick={() => setActiveOption("Content")}
-            className={`${activeOption === "Content" ? "bg-slate-300 text-gray-600" : ""} bg-brown-500 w-fit text-white p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Analytics
-          </button>
-          <button
-            onClick={() => setActiveOption("DeFi")}
-            className={`${activeOption === "DeFi" ? "bg-slate-300 text-gray-600" : ""} bg-brown-500 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Community
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-brown-500 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Content
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-brown-500 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Development
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-brown-500 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Other
-          </button>
+          {
+            type?.map((x) => {
+              return (<button
+                onClick={() => {
+                  if (activeType?.includes(`${x}`)) {
+                    activeType.splice(activeType.indexOf(`${x}`), 1);
+                  }
+                  else {
+                    setActiveType(prev => [...prev, `${x}`])
+                  }
+                }
+                }
+                className={`${activeType?.includes(`${x}`) ? "bg-black text-gray-600" : "bg-brown-500"}    text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
+              >
+                {x}
+              </button>)
+            })
+          }
+
+
         </div>
       </div>
 
@@ -182,114 +85,25 @@ function Filter({ resources, setActiveOption, setFiltered, activeOption }) {
         <h1>Ecosystem</h1>
         <div className="flex flex-wrap flex-col sm:flex-row	">
 
-          <button
-            onClick={() => setActiveOption("Content")}
-            className={`${activeOption === "Content" ? "bg-slate-300 text-gray-600" : ""} bg-purple-900 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Aurora
-          </button>
-          <button
-            onClick={() => setActiveOption("DeFi")}
-            className={`${activeOption === "DeFi" ? "bg-slate-300 text-gray-600" : ""} bg-purple-900 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Avalanche
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-purple-900 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Bitcoin
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-purple-900 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            BNB
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-purple-900 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Celo
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-purple-900 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Cosmo Hub
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-purple-900 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Ethereum
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-purple-900 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Evmos
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-purple-900 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Fantom
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-purple-900 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Gnosis
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-purple-900 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Harmony
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-purple-900 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Intenet Computer
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-purple-900 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Klaytn
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-purple-900 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            NEAR Protocol
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-purple-900 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Optimisim
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-purple-900 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Polygon
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-purple-900 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Solana
-          </button>
-          <button
-            onClick={() => setActiveOption("github")}
-            className={`${activeOption === "github" ? "bg-slate-300 text-gray-600" : ""} bg-purple-900 text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
-          >
-            Stacks
-          </button>
+
+          {
+            ecosystem?.map((x) => {
+              return (<button
+                onClick={() => {
+                  if (activeEcosystem?.includes(`${x}`)) {
+                    activeEcosystem.splice(activeEcosystem.indexOf(`${x}`), 1);
+                  }
+                  else {
+                    setActiveEcosystem(prev => [...prev, `${x}`])
+                  }
+                }
+                }
+                className={`${activeEcosystem?.includes(`${x}`) ? "bg-black text-gray-600" : "bg-purple-500 "}   text-white w-fit p-2 ml-1 mb-2 sm:ml-2 rounded-md border border-gray-300 dark:border-gray-800`}
+              >
+                {x}
+              </button>)
+            })
+          }
 
         </div>
       </div>
--- components/main.jsx
+++ components/main.jsx
@@ -5,9 +5,29 @@ import Filter from "./filter";
 import { useState } from "react";
 
 export default function Main({ data }) {
-    const resource = null;
-    const [filtered, setFiltered] = useState(resource);
-    const [activeOption, setActiveOption] = useState("all");
+
+    const [q, setQ] = useState("");
+    const [filtered, setFiltered] = useState(data);
+    const [activeOption, setActiveOption] = useState([]);
+    const [activeType, setActiveType] = useState([]);
+    const [activeEcosystem, setActiveEcosystem] = useState([]);
+    const [searchParam] = useState(["project"]);
+    const [searchParam2] = useState(["category"]);
+
+    console.log("data!!!!!!", filtered);
+
+    function search(items) {
+        return items?.filter((item) => {
+            return (searchParam).some((newItem) => {
+                return (
+                    item[newItem]?.toString().toLowerCase().indexOf(q.toLowerCase()) >
+                    -1
+                );
+            });
+        }
+        );
+    }
+
     return (
 
         <div className="flex items-center justify-center flex-col mt-4">
@@ -16,20 +36,38 @@ export default function Main({ data }) {
                 <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                 </div>
-                <input type="search" id="default-search" class="block w-[50rem] p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." />
+                <input
+                    type="search"
+                    id="default-search"
+                    class="block w-[50rem] p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    value={q}
+                    placeholder="Search Mockups, Logos..."
+                    onChange={(e) => {
+                        setQ(e.target.value);
+                    }}
+                    onKeyDown={e => {
+                        if (e.key === "Escape") {
+                            setQ("")
+                        }
+                    }}
+                />
             </div>
             <div>
                 <div>
                     <Filter
-                        resources={resource}
+                        filtered={filtered}
                         setFiltered={setFiltered}
                         activeOption={activeOption}
                         setActiveOption={setActiveOption}
+                        activeType={activeType}
+                        setActiveType={setActiveType}
+                        activeEcosystem={activeEcosystem}
+                        setActiveEcosystem={setActiveEcosystem}
                     />
                 </div>
             </div>
             <div>
-                <Cards data={data}/>
+                <Cards data={search(filtered)} />
             </div>
         </div>
 

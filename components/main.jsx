1311ee43f3e9a214907bb43d860f583be3977352
Anjy Gupta <anjyofficial@gmail.com>
2023-04-27 00:38:57 +0530

    fixed gpt

--- components/main.jsx
+++ components/main.jsx
@@ -47,7 +47,7 @@ export default function Main({ data }) {
                 <input
                     type="search"
                     id="default-search"
-                    class="opacity-10 focus:outline-none placeholder-black block w-[50rem] p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    class="opacity-10 focus:outline-none placeholder-black block w-[50rem] p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-[15rem] bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     value={q}
                     placeholder="Search Ecosystem..."
                     onChange={(e) => {

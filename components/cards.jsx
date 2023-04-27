61178d51e206b377c2cdc5dd54ffe0f78508ec19
Anjy Gupta <anjyofficial@gmail.com>
2023-04-28 02:10:15 +0530

    fixed

--- components/cards.jsx
+++ components/cards.jsx
@@ -9,11 +9,12 @@ export default function Cards({ data,activeOption }) {
 
     const array = ["https://uploads-ssl.webflow.com/61d0960dc36814254b73e5ba/63c58f8681ed581fe75226c7_0.png","https://uploads-ssl.webflow.com/61d0960dc36814254b73e5ba/63d020c525b1abb082789af5_0.png","https://uploads-ssl.webflow.com/61d0960dc36814254b73e5ba/63c58f8681ed581fe75226c7_0.png","https://uploads-ssl.webflow.com/61d0960dc36814254b73e5ba/63c45a1274f934af80c1f810_0.png","https://uploads-ssl.webflow.com/61d0960dc36814254b73e5ba/63cd786ac8b33af8c01bfc3c_0.png"]
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:ml-10 xl:ml-0 xl:grid-cols-4 gap-14">
+        <div className="flex items-center">
             {
                 data?.length == 0 || data == null? <div className="text-white">No matching results</div>
                 :
-                data?.map((x) => {
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:ml-10 xl:ml-0 xl:grid-cols-4 gap-14">
+               {data?.map((x) => {
                     {/* console.log(x) */}
                     let imgUrl = array[Math.floor(array.length * Math.random())];
                     return  (
@@ -21,7 +22,8 @@ export default function Cards({ data,activeOption }) {
                             <Card project={x.project} name={x.name} details={x.details} type={x.type} category={x.tags} deadline={x.deadline} imgUrl={x.img} funding={x.funding} time={x.time}/>
                         </a>
                     )
-                })
+               })}
+                </div>
             }
         </div>
     )

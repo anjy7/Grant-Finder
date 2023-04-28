03a22fea9f91d6b35ad18cffbe497270e2a2b335
Anjy Gupta <anjyofficial@gmail.com>
2023-04-28 16:57:48 +0530

    removed console logs

--- components/filter.jsx
+++ components/filter.jsx
@@ -70,7 +70,7 @@ function Filter({ data,filterItem, filtered, setFiltered, activeCat, setActiveCa
                     if (activeOption?.includes(`${x}`)) {
 
                       activeOption.splice(activeOption.indexOf(`${x}`), 1);
-                      console.log("1010100101", activeOption);
+                      // console.log("1010100101", activeOption);
                       let yo = activeOption;
                       activeCat.splice(activeCat.indexOf(`${x}`), 1)
 

c070037306efa408709126d40714ba102d8f3294
Pratyush <connect2pratyush@gmail.com>
2023-04-30 02:05:22 +0530

    count

--- components/main.jsx
+++ components/main.jsx
@@ -23,51 +23,57 @@ export default function Main({ data,activeOption,setActiveOption,filterItem
     const [activeEcosystem, setActiveEcosystem] = useState([]);
     const [searchParam] = useState(["project"]);
     const [searchParam2] = useState(["category"]);
-    
+    // const [howMany, setHowMany] = useState();
 
 
     useEffect(() => {
-        // console.log("first");
-    }, [activeOption])
+        // console.log(filtered);
+    }, [filtered])
 
+    let howMany = 0;
 
     function search(items) {
-        return items?.filter((item) => {
+        let filtItems = items?.filter((item) => {
             return (searchParam).some((newItem) => {
                 return (
-                    item[newItem]?.toString().toLowerCase().indexOf(q.toLowerCase()) >
-                    -1
+                    item[newItem]?.toString().toLowerCase().indexOf(q.toLowerCase()) > -1
                 );
             });
         }
         );
+        howMany = filtItems.length;
+        return filtItems
     }
 
-    
+    const filtData = search(filtered)
 
     return (
 
         <div className="flex items-center justify-center flex-col overflow-x-hidden bg-[#090A0D] mb-10">
-
-            <div class="relative">
-                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+            <div className="flex flex-row">
+                <div className="text-white align-center flex rounded-3xl bg-[#2c1f76] mx-3 px-3">
+                    <text className="my-auto">{howMany}</text>
                 </div>
-                <input
-                    type="search"
-                    id="default-search"
-                    class="bg-opacity-10 focus:outline-none placeholder-white block md:w-[50rem] p-4 pl-10 text-sm text-white border border-gray-300 rounded-[15rem] bg-gray-50 dark:bg-gray-700 border-none dark:border-gray-600 dark:placeholder-white dark:text-white"
-                    value={q}
-                    placeholder="Search"
-                    onChange={(e) => {
-                        setQ(e.target.value);
-                    }}
-                    onKeyDown={e => {
-                        if (e.key === "Escape") {
-                            setQ("")
-                        }
-                    }}
-                />
+                <div class="relative">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                    </div>
+                    <input
+                        type="search"
+                        id="default-search"
+                        class="bg-opacity-10 focus:outline-none placeholder-white block md:w-[50rem] p-4 pl-10 text-sm text-white border border-gray-300 rounded-[15rem] bg-gray-50 dark:bg-gray-700 border-none dark:border-gray-600 dark:placeholder-white dark:text-white"
+                        value={q}
+                        placeholder={`Search Grants`}
+                        onChange={(e) => {
+                            setQ(e.target.value);
+                        }}
+                        onKeyDown={e => {
+                            if (e.key === "Escape") {
+                                setQ("")
+                            }
+                        }}
+                    />
+                </div>
             </div>
             <div>
                 {
@@ -123,7 +129,7 @@ export default function Main({ data,activeOption,setActiveOption,filterItem
                 }
             </div>
             <div>
-                <Cards data={search(filtered)}/>
+                <Cards data={filtData}/>
             </div>
         </div>
 
